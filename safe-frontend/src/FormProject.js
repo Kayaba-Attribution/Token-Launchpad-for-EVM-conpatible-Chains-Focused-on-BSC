39a8f3265ce998c0dadfa5e8a185026e7d0276af
Juan Gomez <jdavidgomezca@gmail.com>
2021-08-25 19:08:46 -0400

    Form Updated

--- safe-frontend/src/FormProject.js
+++ safe-frontend/src/FormProject.js
@@ -16,16 +16,18 @@ margin-left: 3vw;
 
 const FormProject = () => {
 	const [name, setName] = useState('');
-	const [symbol, setSymbol] = useState('');
-	const [supply, setSupply] = useState('');
+	const [address, setAddress] = useState('');
+	const [minimum, setMinimum] = useState('');
+	const [maximum, setMaximum] = useState('');
+	const [rate, setRate] = useState('');
 	const [presale, setPresale] = useState('');
-	const [softcap, setSoftcap] = useState('');
+	const [lockTime, setLockTime] = useState('');
 	const [sent, setSent] = useState(false);
 	const sheet='https://sheet.best/api/sheets/25a1b0be-167a-4e34-b294-0b8f73d277c0';
 	const handleSubmit = (e) => {
 		e.preventDefault();
 
-		const object = { name, symbol, supply, presale, softcap };
+		const object = { name, address, minimum, maximum, rate, presale, lockTime };
 
 		axios.post(sheet, object)
 		.then((response) => {
@@ -43,21 +45,29 @@ const FormProject = () => {
             <label>Name</label>
             <input placeholder='Enter project name' onChange={(e) => setName(e.target.value)}/>
           </Form.Field>
-          <Form.Field>
-            <label>Symbol</label>
-            <input placeholder='Enter project symbol' onChange={(e) => setSymbol(e.target.value)}/>
+		  <Form.Field>
+            <label>Token Address</label>
+            <input placeholder='Enter token address' onChange={(e) => setAddress(e.target.value)}/>
           </Form.Field>
           <Form.Field>
-            <label>Supply</label>
-            <input placeholder='Enter project supply' onChange={(e) => setSupply(e.target.value)} />
+            <label>Minimum Contribution (BNB)</label>
+            <input placeholder='Enter project minimum contribution' onChange={(e) => setMinimum(e.target.value)}/>
+          </Form.Field>
+		  <Form.Field>
+            <label>Maximum Contribution (BNB)</label>
+            <input placeholder='Enter project maximum contribution' onChange={(e) => setMaximum(e.target.value)}/>
           </Form.Field>
           <Form.Field>
-            <label>Presale</label>
+            <label>Pre-sale rate (amount of tokens user gets for a deposit of 1 bnb)</label>
+            <input placeholder='Enter project pre-sale rate' onChange={(e) => setRate(e.target.value)} />
+          </Form.Field>
+          <Form.Field>
+            <label>Presale Cap</label>
             <input placeholder='Enter presale cap' onChange={(e) => setPresale(e.target.value)}/>
           </Form.Field>
           <Form.Field>
-            <label>Softcap</label>
-            <input placeholder='Enter softcap' onChange={(e) => setSoftcap(e.target.value)}/>
+            <label>Liquidity Lock Time (months)</label>
+            <input placeholder='Enter Liquidity Lock Up Time' onChange={(e) => setLockTime(e.target.value)}/>
           </Form.Field>
           
           <Button color="blue" type='submit' onClick={handleSubmit}>Submit</Button>
