7f6710395c6f2fda0baecb435400f854a9459ba8
Juan Gomez <jdavidgomezca@gmail.com>
2021-08-27 06:38:26 -0400

    Presale contract peer programming

--- safe-frontend/src/contracts/pre-sale.sol
+++ safe-frontend/src/contracts/pre-sale.sol
@@ -1,3 +1,7 @@
+/**
+ *Submitted for verification at BscScan.com on 2021-08-27
+*/
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
@@ -17,109 +21,102 @@ interface IERC20 {
 
 contract preSale {
 
-    address payable public owner;
+    address public owner;
 
-    address private tokenAddress;
-    uint private rate;
-    uint private cap = 1 ether;
-    uint private minBNBContribution = 0.1 ether;
-    uint private maxBNBContribution = 1 ether;
+    // preSale static information
+    address public tokenAddress;
+    //uint public immutable rate;
+    uint public immutable cap;
+    uint public immutable minBNBContribution;
+    uint public immutable maxBNBContribution;
 
-    uint private tokensAvaliable;
-    uint private weiRaised;
-    bool private capReached = false;
+    // preSale dynamic information
+    uint public tokensAvaliable;
+    uint public weiRaised;
+    bool public capReached = false;
 
-    mapping(address => uint) private contributions;
-    mapping ( address => uint256 ) public t_balances;
+    // contribution (bnb) mapping, indexing, and number of contributors 
+    uint total_contributors = 0;
+    mapping (address => uint) public contributions;
+    mapping (uint => address) private contributor_indices;
+   // mapping (address => uint) public t_balances;
 
-    function SeeTokenAddress() public view returns (address) {
-        return tokenAddress;
+    constructor (uint _cap, uint _minBNB, uint _maxBNB) {
+        cap = _cap;
+        minBNBContribution = _minBNB;
+        maxBNBContribution = _maxBNB;
+        owner = msg.sender;
     }
-
-    function approveTokens(uint tokens) public {
-
-        // approve the tokens from the sender to this contract
-        IERC20(tokenAddress).approve(address(this), tokens);
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
     }
+    
 
-    function seeAllowance(address _owner) public view returns (uint) {
-
-        // approve the tokens from the sender to this contract
-        return IERC20(tokenAddress).allowance(_owner, address(this));
-    }
-
-    function seeBalance( address _addr) public view returns (uint) {
-        return IERC20(tokenAddress).balanceOf(_addr);
-    }
-
-    function depositTokens(uint tokens) public {
-
-        // add the deposited tokens into existing balance 
-        t_balances[msg.sender]+= tokens;
-
-        // transfer the tokens from the sender to this contract
-        IERC20(tokenAddress).transferFrom(msg.sender, address(this), tokens);
-    }
-
-    function returnTokens() public {
-        uint256 amount = t_balances[msg.sender];
-        t_balances[msg.sender] = 0;
-        IERC20(tokenAddress).transfer(msg.sender, amount);
-    }
-
-    function SeeRate() public view returns (uint) {
-        return rate;
-    }
-
-    function SeeCap() public view returns (uint) {
-        return cap;
-    }
-
-    function seeMinContribution() public view returns (uint) {
-        return minBNBContribution;
-    }
-
-    function seeMaxContribution() public view returns (uint) {
-        return maxBNBContribution;
-    }
-
-    function SeeWeiRaised() public view returns (uint) {
-        return weiRaised;
-    }
-
-    function SeeAddressContribution(address _addr) public view returns (uint) {
-        return contributions[_addr];
-    }
-
-    function capReached_() public view returns (bool) {
-        return capReached;
-    }
-
-    constructor() payable{
-        
-        owner = payable(msg.sender);
-    }
 
     function setTokenAddress(address _addr) public {
         // Update the value at this address
         tokenAddress = _addr;
     }
 
-    function deposit(uint amount) public payable {
-        require(msg.value == amount);
-        require(msg.value >= 0.1 ether, "Deposit Value is Too Small ");
-        require(msg.value <= 1 ether, "Deposit Value is Too Big");
-        require(capReached == false, "Cap is already reached");
 
+    function SeeAddressContribution(address _addr) external view returns (uint) {
+        return contributions[_addr];
+    }
+    
 
-        weiRaised += amount;
+    
+    function calculateOffset(uint _value) pure private returns ( uint ) {
+        // store the calculation in a big enough value
+        uint result = ( 99 * _value ) / 100;
+
+        // convert result
+        return uint( result );
+    }
+
+    function deposit() public payable {
+        require(msg.value >= minBNBContribution, "Deposit Value is Too Small");
+        require(msg.value <= maxBNBContribution, "Deposit Value is Too Big");
+        require(!capReached, "Cap is already reached");
+
+        weiRaised += msg.value;
         require(weiRaised <= cap, "Reverted: BNB deposit would go over cap");
-
-        if (weiRaised == cap){
+        
+        // 1% offset see calculateOffset for details
+        if (weiRaised >= calculateOffset(cap)){
             capReached = true;
         }
 
-        contributions[msg.sender] = amount;
+        bool found = false;
+
+        for(uint i = 0; i < total_contributors; i++)
+        {
+            if(contributor_indices[i] == msg.sender)
+            {
+                found = true;
+            }
+        }
+
+        require(found == false, "You have already contributed to the presale");
+        contributor_indices[total_contributors] = msg.sender;
+        contributions[msg.sender] = msg.value;
+        total_contributors++;
+    }
+
+    function send_BNB_back() external onlyOwner {
+        for (uint i = 0 ; i < total_contributors; i++) {
+            // use inner mapping to get address, get bnb amount with address, send back that bnb value
+            address temp_A = contributor_indices[i];
+            uint temp_V = contributions[contributor_indices[i]];
+            (bool success, ) = payable(temp_A).call{value:temp_V}("");
+            require(success, "Failed to send Ether");
+            // decrease weiRaised and delete the mappings !! THIS IS ONLY EXPERIMENTAL (CONTRACT RE-USE) !!
+            weiRaised -= temp_V;
+            delete contributor_indices[i];
+            delete contributions[temp_A];
+            
+        }
     }
 
     function withdraw() public onlyOwner {
@@ -128,14 +125,38 @@ contract preSale {
 
         // send all Ether to owner
         // Owner can receive Ether since the address of owner is payable
-        (bool success, ) = owner.call{value: amount}("");
+        (bool success, ) = payable(owner).call{value: amount}("");
         require(success, "Failed to send Ether");
     }
 
+    
+        // function approveTokens(uint tokens) public {
+    //     // approve the tokens from the sender to this contract
+    //     IERC20(tokenAddress).approve(address(this), tokens);
+    // }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not owner");
-        _;
-    }
+    // function seeAllowance(address _owner) public view returns (uint) {
+    //     // approve the tokens from the sender to this contract
+    //     return IERC20(tokenAddress).allowance(_owner, address(this));
+    // }
+
+    // function seeBalance( address _addr) public view returns (uint) {
+    //     return IERC20(tokenAddress).balanceOf(_addr);
+    // }
+
+    // function depositTokens(uint tokens) public {
+
+    //     // add the deposited tokens into existing balance 
+    //     t_balances[msg.sender]+= tokens;
+
+    //     // transfer the tokens from the sender to this contract
+    //     IERC20(tokenAddress).transferFrom(msg.sender, address(this), tokens);
+    // }
+
+    // function returnTokens() public {
+    //     uint256 amount = t_balances[msg.sender];
+    //     t_balances[msg.sender] = 0;
+    //     IERC20(tokenAddress).transfer(msg.sender, amount);
+    // }
 
 }
